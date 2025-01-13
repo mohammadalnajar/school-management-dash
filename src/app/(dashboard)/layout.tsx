b42c609e037a14759c24018ecba15ef11b804839
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2025-01-13 22:58:42 +0100

    add dashboard layout and pages for admin, parent, student, and teacher

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,33 @@
+import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function DashboardLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className='h-screen flex'>
+            {/* LEFT */}
+            <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[16%]  p-4'>
+                <Link
+                    className='flex items-center justify-center lg:justify-start gap-2'
+                    href='/dashboard'
+                >
+                    <Image src='/logo.png' alt='logo' width={32} height={32} />
+                    <span className='hidden lg:block'>SchoolNajar</span>
+                </Link>
+
+                <Menu />
+            </div>
+            {/* RIGHT */}
+            <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[84%] bg-[#F7F8FA] overflow-scroll'>
+                <Navbar />
+
+                {children}
+            </div>
+        </div>
+    );
+}
