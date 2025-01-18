17cd2213f9d88d695d0f2e27815ef97b8ec2f0ba
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2025-01-18 13:44:46 +0100

    fix: standardize padding formatting in BigCalendar component

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -22,7 +22,7 @@ const BigCalendar = () => {
             backgroundColor,
             color: textColor, // Adjust text color for better contrast
             border: 'none',
-            padding:'10px' ,
+            padding: '10px',
             margin: '10px',
             width: '99%'
         };
