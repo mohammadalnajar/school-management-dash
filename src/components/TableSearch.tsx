7b1d4bad86638acf18f6962c6d4c1ef5c763f64c
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2025-03-07 15:39:04 +0100

    feat: implement search functionality in TableSearch component

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,6 +1,20 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const TableSearch = () => {
+    const router = useRouter();
+    const [search, setSearch] = useState('');
+
+    const changeSearch = () => {
+        const params = new URLSearchParams(window.location.search);
+
+        params.set('search', search);
+        router.push(`${window.location.pathname}?${params.toString()}`);
+    };
+
     return (
         <div className='w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2'>
             <Image src='/search.png' alt='' width={14} height={14} />
@@ -8,6 +22,13 @@ const TableSearch = () => {
                 type='text'
                 placeholder='Search...'
                 className='w-[200px] p-2 bg-transparent outline-none'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyUp={(e) => {
+                    if (e.key === 'Enter') {
+                        changeSearch();
+                    }
+                }}
             />
         </div>
     );
