e80cda3246141e3f23b7961677f357f1dd8d215e
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2025-03-07 16:53:36 +0100

    feat: implement server-side pagination and search functionality in StudentListPage

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -2,7 +2,10 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role, studentsData } from '@/lib/data';
+import { role } from '@/lib/data';
+import prisma from '@/lib/prisma';
+import { ITEMS_PER_PAGE } from '@/lib/settings';
+import { Prisma } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -48,47 +51,95 @@ const columns = [
         accessor: 'action'
     }
 ];
+const renderRow = (item: Student) => (
+    <tr
+        key={item.id}
+        className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-smdPurpleLight'
+    >
+        <td className='flex items-center gap-4 p-4'>
+            <Image
+                src={item.photo}
+                alt=''
+                width={40}
+                height={40}
+                className='md:hidden xl:block w-10 h-10 rounded-full object-cover'
+            />
+            <div className='flex flex-col'>
+                <h3 className='font-semibold'>{item.name}</h3>
+                <p className='text-xs text-gray-500'>{item.class}</p>
+            </div>
+        </td>
+        <td className='hidden md:table-cell'>{item.studentId}</td>
+        <td className='hidden md:table-cell'>{item.grade}</td>
+        <td className='hidden md:table-cell'>{item.phone}</td>
+        <td className='hidden md:table-cell'>{item.address}</td>
+        <td>
+            <div className='flex items-center gap-2'>
+                <Link href={`/list/teachers/${item.id}`}>
+                    <button className='w-7 h-7 flex items-center justify-center rounded-full bg-smdSkyBlue'>
+                        <Image src='/view.png' alt='' width={16} height={16} />
+                    </button>
+                </Link>
+                {role === 'admin' && (
+                    // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-smdPurple">
+                    //   <Image src="/delete.png" alt="" width={16} height={16} />
+                    // </button>
+                    <FormModal table='student' type='delete' id={item.id} />
+                )}
+            </div>
+        </td>
+    </tr>
+);
 
-const StudentListPage = () => {
-    const renderRow = (item: Student) => (
-        <tr
-            key={item.id}
-            className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-smdPurpleLight'
-        >
-            <td className='flex items-center gap-4 p-4'>
-                <Image
-                    src={item.photo}
-                    alt=''
-                    width={40}
-                    height={40}
-                    className='md:hidden xl:block w-10 h-10 rounded-full object-cover'
-                />
-                <div className='flex flex-col'>
-                    <h3 className='font-semibold'>{item.name}</h3>
-                    <p className='text-xs text-gray-500'>{item.class}</p>
-                </div>
-            </td>
-            <td className='hidden md:table-cell'>{item.studentId}</td>
-            <td className='hidden md:table-cell'>{item.grade}</td>
-            <td className='hidden md:table-cell'>{item.phone}</td>
-            <td className='hidden md:table-cell'>{item.address}</td>
-            <td>
-                <div className='flex items-center gap-2'>
-                    <Link href={`/list/teachers/${item.id}`}>
-                        <button className='w-7 h-7 flex items-center justify-center rounded-full bg-smdSkyBlue'>
-                            <Image src='/view.png' alt='' width={16} height={16} />
-                        </button>
-                    </Link>
-                    {role === 'admin' && (
-                        // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-smdPurple">
-                        //   <Image src="/delete.png" alt="" width={16} height={16} />
-                        // </button>
-                        <FormModal table='student' type='delete' id={item.id} />
-                    )}
-                </div>
-            </td>
-        </tr>
-    );
+const StudentListPage = async ({
+    searchParams
+}: {
+    searchParams: { [key: string]: string | undefined };
+}) => {
+    const { page, ...queryParams } = searchParams;
+
+    const p = Number(page) || 1;
+
+    // URL PARAMS CONDITIONS
+    const query: Prisma.StudentWhereInput = {};
+
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value) {
+                switch (key) {
+                    // case 'classId':
+                    //     query.lessons = {
+                    //         some: {
+                    //             classId: Number(value)
+                    //         }
+                    //     };
+                    //     break;
+
+                    case 'search':
+                        query.name = {
+                            contains: value,
+                            mode: 'insensitive'
+                        };
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    const [students, count] = await prisma.$transaction([
+        prisma.student.findMany({
+            where: query,
+
+            take: ITEMS_PER_PAGE,
+            skip: ITEMS_PER_PAGE * (p - 1)
+        }),
+        prisma.student.count({
+            where: query
+        })
+    ]);
 
     return (
         <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
@@ -114,9 +165,9 @@ const StudentListPage = () => {
                 </div>
             </div>
             {/* LIST */}
-            <Table columns={columns} renderRow={renderRow} data={studentsData} />
+            <Table columns={columns} renderRow={renderRow} data={students} />
             {/* PAGINATION */}
-            <Pagination />
+            <Pagination page={p} count={count} />
         </div>
     );
 };
