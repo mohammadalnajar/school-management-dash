7e38b33f59890764674452653beb9b60e862ad7d
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2025-03-07 17:58:14 +0100

    feat: implement server-side pagination and search functionality in ClassListPage

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -3,15 +3,12 @@ import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
 import { classesData, role } from '@/lib/data';
+import prisma from '@/lib/prisma';
+import { ITEMS_PER_PAGE } from '@/lib/settings';
+import { Class, Grade, Prisma, Teacher } from '@prisma/client';
 import Image from 'next/image';
 
-type Class = {
-    id: number;
-    name: string;
-    capacity: number;
-    grade: number;
-    supervisor: string;
-};
+type ClassList = Class & { grade: Grade; supervisor: Teacher };
 
 const columns = [
     {
@@ -39,28 +36,77 @@ const columns = [
     }
 ];
 
-const ClassListPage = () => {
-    const renderRow = (item: Class) => (
-        <tr
-            key={item.id}
-            className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-smdPurpleLight'
-        >
-            <td className='flex items-center gap-4 p-4'>{item.name}</td>
-            <td className='hidden md:table-cell'>{item.capacity}</td>
-            <td className='hidden md:table-cell'>{item.grade}</td>
-            <td className='hidden md:table-cell'>{item.supervisor}</td>
-            <td>
-                <div className='flex items-center gap-2'>
-                    {role === 'admin' && (
-                        <>
-                            <FormModal table='class' type='update' data={item} />
-                            <FormModal table='class' type='delete' id={item.id} />
-                        </>
-                    )}
-                </div>
-            </td>
-        </tr>
-    );
+const renderRow = (item: ClassList) => (
+    <tr
+        key={item.id}
+        className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-smdPurpleLight'
+    >
+        <td className='flex items-center gap-4 p-4'>{item.name}</td>
+        <td className='hidden md:table-cell'>{item.capacity}</td>
+        <td className='hidden md:table-cell'>{item.grade.level}</td>
+        <td className='hidden md:table-cell'>{item.supervisor.name}</td>
+        <td>
+            <div className='flex items-center gap-2'>
+                {role === 'admin' && (
+                    <>
+                        <FormModal table='class' type='update' data={item} />
+                        <FormModal table='class' type='delete' id={item.id} />
+                    </>
+                )}
+            </div>
+        </td>
+    </tr>
+);
+
+const ClassListPage = async ({
+    searchParams
+}: {
+    searchParams: { [key: string]: string | undefined };
+}) => {
+    const { page, ...queryParams } = searchParams;
+
+    const p = Number(page) || 1;
+
+    // URL PARAMS CONDITIONS
+    const query: Prisma.ClassWhereInput = {};
+
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value) {
+                switch (key) {
+                    case 'supervisorId':
+                        query.supervisorId = value;
+                        break;
+
+                    case 'search':
+                        query.name = {
+                            contains: value,
+                            mode: 'insensitive'
+                        };
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    const [classes, count] = await prisma.$transaction([
+        prisma.class.findMany({
+            where: query,
+            include: {
+                grade: true,
+                supervisor: true
+            },
+
+            take: ITEMS_PER_PAGE,
+            skip: ITEMS_PER_PAGE * (p - 1)
+        }),
+        prisma.class.count({
+            where: query
+        })
+    ]);
 
     return (
         <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
@@ -81,9 +127,9 @@ const ClassListPage = () => {
                 </div>
             </div>
             {/* LIST */}
-            <Table columns={columns} renderRow={renderRow} data={classesData} />
+            <Table columns={columns} renderRow={renderRow} data={classes} />
             {/* PAGINATION */}
-            <Pagination />
+            <Pagination page={p} count={count} />
         </div>
     );
 };
